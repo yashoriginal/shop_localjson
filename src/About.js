421d821748ac7eba2bb0e4da2ef3d3c09b1b11ec
unknown <yaswanth.suriya@outlook.com>
2025-02-16 12:44:44 +0530

    Added bg color

--- src/About.js
+++ src/About.js
@@ -1,17 +1,15 @@
 const About = () => {
   return (
-    <div className="container mx-auto p-6 text-gray-800">
-      <h2 className="text-3xl font-bold text-center mb-4">
-        About Jewel Ledger
-      </h2>
-      <p className="text-lg text-center">
+    <div className="about">
+      <h2>About Jewel Ledger</h2>
+      <p>
         Jewel Ledger is a powerful and easy-to-use application designed for
         jewelry loan shops. It helps manage customer bills, calculate interest,
         and keep track of gold and silver jewelry loans efficiently. Our goal is
         to simplify financial record-keeping, ensuring accurate and hassle-free
         loan management.
       </p>
-      <p className="text-lg text-center mt-4">
+      <p>
         Whether you are handling gold or silver transactions, Jewel Ledger
         provides a seamless solution to keep your business organized and running
         smoothly.
